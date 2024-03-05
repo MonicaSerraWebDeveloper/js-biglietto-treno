90927e6ebe6a3d4df4defc4c25c63d6c04d1bafe
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-03-05 14:36:49 +0100

    feat: definition of the execution as a comment

--- js/script.js
+++ js/script.js
@@ -4,3 +4,12 @@
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
 // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). 
+
+
+// Chiediamo all'utente il numero di km (prompt)
+// Chiediamo all'utente la sua età (prompt numerico)
+// Calcolo matematico in base ai km (0.21 * km)
+// Calcolo della percentuale del 20%
+// Calcolo della percentuale del 40%
+// Definizione della condizione per le percentuali in base all'età
+// Prezzo totale del biglietto finale con massimo due decimali
