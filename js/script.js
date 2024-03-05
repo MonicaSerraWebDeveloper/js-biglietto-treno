60b32540a64441085f0538c22e1d467316a5507e
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-03-05 17:35:37 +0100

    fix: clean the code in js deleted all console.log and the extra comment on the top of page

--- js/script.js
+++ js/script.js
@@ -1,22 +1,11 @@
-// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
-// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
-// il prezzo del biglietto è definito in base ai km (0.21 € al km)
-// va applicato uno sconto del 20% per i minorenni
-// va applicato uno sconto del 40% per gli over 65.
-// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). 
-
-
 // Chiediamo all'utente il numero di km (prompt numerico)
 const userKm = parseInt (prompt('Quanti km devi fare?'));
-console.log(userKm);
 
 // Chiediamo all'utente la sua età (prompt numerico)
 const userAge = parseInt (prompt('Quanti anni hai?'));
-console.log(userAge);
 
 // Calcolo matematico in base ai km (0.21 * km)
 const priceTrip = userKm * 0.21;
-console.log(priceTrip);
 
 // Calcolo della percentuale del 20%
 const discountTwenty = (priceTrip * 20) / 100;
@@ -41,7 +30,6 @@ if (userAge < 18) {
 
 // Prezzo totale del biglietto finale con massimo due decimali
 let priceTotalTwoDecimal = parseFloat (priceTicketTotal.toFixed(2));
-console.log(priceTotalTwoDecimal);
 
 // Stampiamo nella pagina HTML il prezzo finale con due decimali
 const messageTotalPrice = `Il prezzo del tuo biglietto è: €${priceTotalTwoDecimal}`;
